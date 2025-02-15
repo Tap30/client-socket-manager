3e8a5f76cc6e9017fd4ec41336b1b038bccbf6e8
mimshins <mostafa.sh.coderino@gmail.com>
2025-02-15 15:35:30 +0330

    feat(api): expose event listeners types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { default as ClientSocketManager } from "./ClientSocketManager.ts";
 export { ManagerReservedEvents, SocketReservedEvents } from "./constants.ts";
-export type { ClientSocketManagerOptions } from "./types.ts";
+export type {
+  ClientSocketManagerListenerOptions,
+  ClientSocketManagerOptions,
+} from "./types.ts";
