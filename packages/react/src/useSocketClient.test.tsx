6ae4312b872b0db5302150275aab18c4db3cc79a
mimshins <mostafa.sh.coderino@gmail.com>
2025-02-26 15:54:39 +0330

    add tests

---
+++ packages/react/src/useSocketClient.test.tsx
@@ -0,0 +1,154 @@
+import {
+  MockClientSocketManager as __MockClientSocketManager__,
+  act,
+  render,
+  screen,
+} from "@repo/test-helpers";
+import {
+  ClientSocketManager,
+  type ClientSocketManagerOptions,
+} from "@tapsioss/client-socket-manager";
+import {
+  beforeEach,
+  describe,
+  expect,
+  it,
+  vitest,
+  type MockedClass,
+} from "vitest";
+import { SocketContext } from "./Context.ts";
+import SocketClientProvider from "./SocketClientProvider.tsx";
+import { ConnectionStatus } from "./constants.ts";
+import useSocketClient from "./useSocketClient.ts";
+
+// Mock the ClientSocketManager class
+vitest.mock("@tapsioss/client-socket-manager", () => {
+  return {
+    ClientSocketManager: vitest.fn(
+      (uri: string, options?: Partial<ClientSocketManagerOptions>) => {
+        const mock = new __MockClientSocketManager__(uri, options);
+
+        return {
+          get connected() {
+            return mock.connected;
+          },
+          connect() {
+            mock.connect();
+          },
+          disconnect() {
+            mock.disconnect();
+          },
+          dispose() {
+            mock.dispose();
+          },
+        };
+      },
+    ),
+  };
+});
+
+describe("useSocketClient", () => {
+  beforeEach(() => {
+    vitest.clearAllMocks();
+  });
+
+  const socketServerUri = "http://localhost:3000";
+  const MockClientSocketManager = ClientSocketManager as MockedClass<
+    typeof ClientSocketManager
+  >;
+
+  const TestComponent = () => {
+    const context = useSocketClient();
+
+    return (
+      <div>
+        <span data-testid="connection-status">{context.connectionStatus}</span>
+        <span data-testid="socket-instance">
+          {context.socket ? "true" : "false"}
+        </span>
+      </div>
+    );
+  };
+
+  it("should throw error when used outside of SocketClientProvider", () => {
+    expect(() => render(<TestComponent />)).toThrow();
+  });
+
+  it("should use context values when used within SocketClientProvider", () => {
+    const { rerender, unmount } = render(
+      <SocketContext.Provider
+        value={{
+          connectionStatus: ConnectionStatus.DISCONNECTED,
+          socket: null,
+        }}
+      >
+        <TestComponent />
+      </SocketContext.Provider>,
+    );
+
+    expect(screen.getByTestId("connection-status")).toHaveTextContent(
+      ConnectionStatus.DISCONNECTED,
+    );
+    expect(screen.getByTestId("socket-instance")).toHaveTextContent("false");
+
+    rerender(
+      <SocketContext.Provider
+        value={{
+          connectionStatus: ConnectionStatus.CONNECTED,
+          socket: null,
+        }}
+      >
+        <TestComponent />
+      </SocketContext.Provider>,
+    );
+
+    expect(screen.getByTestId("connection-status")).toHaveTextContent(
+      ConnectionStatus.CONNECTED,
+    );
+    expect(screen.getByTestId("socket-instance")).toHaveTextContent("false");
+
+    unmount();
+    render(
+      <SocketClientProvider uri={socketServerUri}>
+        <TestComponent />
+      </SocketClientProvider>,
+    );
+
+    expect(MockClientSocketManager.mock.results[0]).toBeDefined();
+
+    const client = MockClientSocketManager.mock.results[0]!
+      .value as ClientSocketManager;
+
+    expect(screen.getByTestId("connection-status")).toHaveTextContent(
+      ConnectionStatus.DISCONNECTED,
+    );
+    expect(screen.getByTestId("socket-instance")).toHaveTextContent("true");
+
+    act(() => {
+      client.connect();
+    });
+
+    expect(screen.getByTestId("connection-status")).toHaveTextContent(
+      ConnectionStatus.CONNECTED,
+    );
+    expect(screen.getByTestId("socket-instance")).toHaveTextContent("true");
+
+    act(() => {
+      client.disconnect();
+    });
+
+    expect(screen.getByTestId("connection-status")).toHaveTextContent(
+      ConnectionStatus.DISCONNECTED,
+    );
+    expect(screen.getByTestId("socket-instance")).toHaveTextContent("true");
+
+    act(() => {
+      client.dispose();
+    });
+
+    expect(screen.getByTestId("connection-status")).toHaveTextContent(
+      ConnectionStatus.DISCONNECTED,
+    );
+    expect(screen.getByTestId("socket-instance")).toHaveTextContent("true");
+  });
+});
